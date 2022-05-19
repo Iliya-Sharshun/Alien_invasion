770d10374540b0eee26a923ff19637c8a7f04bba
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-19 09:30:12 +0800

    Add method for control keystrocles and mouse events

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -22,14 +22,18 @@ class AlienInvasion:
 		"""Run main game cycle"""
 
 		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					sys.exit()
+			self._check_events()
 
 			self.screen.fill(self.settings.bg_color)
 			self.ship.blitme()
 
 			pygame.display.flip()
+
+	def _check_events(self):
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
 
 if __name__ == '__main__':
 	ai = AlienInvasion()
