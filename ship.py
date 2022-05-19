4e7d53a61ac6a34495dd2f7cfbca065529e19a4b
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-19 14:18:44 +0800

    Add methods for movig ship on the left

--- ship.py
+++ ship.py
@@ -13,12 +13,15 @@ class Ship():
 		self.rect = self.image.get_rect()
 		self.rect.midbottom = self.screen_rect.midbottom
 		self.moving_right = False
+		self.moving_left = False
 
 	def update(self):
 		"""Uodate position ship"""
 
 		if self.moving_right:
 			self.rect.x += 1
+		if self.moving_left:
+			self.rect.x -= 1
 
 	def blitme(self):
 		"""Created ship in current position"""
