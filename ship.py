193c24472c9df518d8488940dd92e12425535a5d
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-19 14:59:58 +0800

    Add setting fo changes sheep speed

--- ship.py
+++ ship.py
@@ -7,11 +7,15 @@ class Ship():
 		"""Initializing ship and created his start position"""
 
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
 		self.rect.midbottom = self.screen_rect.midbottom
+		
+		self.x = float(self.rect.x)
+
 		self.moving_right = False
 		self.moving_left = False
 
@@ -19,9 +23,11 @@ class Ship():
 		"""Uodate position ship"""
 
 		if self.moving_right:
-			self.rect.x += 1
+			self.x += self.settings.ship_speed
 		if self.moving_left:
-			self.rect.x -= 1
+			self.x -= self.settings.ship_speed
+
+		self.rect.x = self.x
 
 	def blitme(self):
 		"""Created ship in current position"""
