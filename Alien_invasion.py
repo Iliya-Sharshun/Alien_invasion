0e39bf8ae749633bf6a27ed2cf7de9e3cb7145d5
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-19 09:34:26 +0800

    Add method for update screen

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -23,17 +23,20 @@ class AlienInvasion:
 
 		while True:
 			self._check_events()
-
-			self.screen.fill(self.settings.bg_color)
-			self.ship.blitme()
-
-			pygame.display.flip()
+			self._update_screen()
 
 	def _check_events(self):
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+
+	def _update_screen(self):
+
+		self.screen.fill(self.settings.bg_color)
+		self.ship.blitme()
+
+		pygame.display.flip()
 
 if __name__ == '__main__':
 	ai = AlienInvasion()
