771777fea43e8f2bcf233014f70b0a14f3514d5f
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-21 18:22:06 +0800

    Add shot processing

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 import pygame
 
@@ -18,6 +19,7 @@ class AlienInvasion:
 		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption('Alien Invasion')
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 		
 
 	def run_game(self):
@@ -26,6 +28,7 @@ class AlienInvasion:
 		while True:
 			self._check_events()
 			self.ship.update()
+			self.bullets.update()
 			self._update_screen()
 
 	def _check_events(self):
@@ -47,6 +50,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		"""Reacts on keyup"""
@@ -57,11 +62,19 @@ class AlienInvasion:
 			self.ship.moving_left = False
 
 
+	def _fire_bullet(self):
+		"""Created new bullet and addhis in group bullets"""
+
+		new_bullet = Bullet(self)
+		self.bullets.add(new_bullet)
 
 	def _update_screen(self):
 
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 		pygame.display.flip()
 
--- bullet.py
+++ bullet.py
@@ -4,26 +4,26 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
 	"""Class for controll bullets that have been fired by a ship"""
 
-		def __init__(self, ai_game):
-			"""Created bullet object"""
+	def __init__(self, ai_game):
+		"""Created bullet object"""
 
-			super.__init__()
-			self,screen = ai_game.screen
-			self.settings = ai_game.settings
-			self.color = self.settings.bullet_color
+		super().__init__()
+		self.screen = ai_game.screen
+		self.settings = ai_game.settings
+		self.color = self.settings.bullet_color
 
-			self.rect = pygame.Rect(0, 0, self.settings.screen_width, self.settings.screen_height)
-			self.rect.midtop = ai_game.ship.rect.midtop
+		self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+		self.rect.midtop = ai_game.ship.rect.midtop
 
-			self.y = float(self.rect.y)
+		self.y = float(self.rect.y)
 
-		def update(self):
-			"""Moved bullet up the screen"""
+	def update(self):
+		"""Moved bullet up the screen"""
 
-			self.y -= self.settings.bullet_speed
-			self.rect.y = self.y
+		self.y -= self.settings.bullet_speed
+		self.rect.y = self.y
 
-		def draw_bullet(self):
-			"""Draw bullet on the screen"""
+	def draw_bullet(self):
+		"""Draw bullet on the screen"""
 
-			pygame.draw.rect(self.screen, self.color, self.rect)
+		pygame.draw.rect(self.screen, self.color, self.rect)
