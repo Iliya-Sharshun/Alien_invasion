c79417e8c624e36cc5de7e5d69079dfdb170aed2
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-25 17:44:34 +0800

    Added collision for aliens ships with player ship

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -15,7 +15,7 @@ class AlienInvasion:
 
 		pygame.init()
 		self.settings = Settings()
-		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		self.settings.screen_width = self.screen.get_rect().width
 		self.settings.screen_height = self.screen.get_rect().height
 		pygame.display.set_caption('Alien Invasion')
@@ -102,6 +102,9 @@ class AlienInvasion:
 
 		self._check_fleet_ebges()
 		self.aliens.update()
+
+		if pygame.sprite.spritecollideany(self.ship, self.aliens):
+			print('ship hit!')
 
 	def _create_fleet(self):
 		"""Created fleet invasion"""
