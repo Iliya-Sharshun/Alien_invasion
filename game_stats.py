8173be9eba5db668b498f5afc50396b2265ad25f
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-05-27 21:13:42 +0800

    Added game over module to display button on the screen

--- game_stats.py
+++ game_stats.py
@@ -7,7 +7,8 @@ class GameStats():
 		self.settings = ai_game.settings
 		self.reset_stats()
 
-		self.game_active = True
+		self.game_active = False
+
 
 	def reset_stats(self):
 		"""Itializes statistics that change guring the game"""
