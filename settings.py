2441b2dfbb6f267f5b621727c396c02049602458
Ilya_Sharshun <Sharshun.Ilya@yandex.ru>
2022-06-04 10:52:47 +0800

    Added dynamic difficulty change

--- settings.py
+++ settings.py
@@ -7,15 +7,32 @@ class Settings():
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
-		self.ship_speed = 1.5
+		
 		self.ship_limit = 3
 
-		self.bullet_speed = 1.5
-		self.bullet_width = 30
+		self.bullet_width = 3000
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
 		self.bullet_allowed = 3
 
-		self.alien_speed = 1.5
 		self.fleet_drop_speed = 10
 		self.fleet_direction = 1
+
+
+		self.speedup_scale = 1.1
+		self.intialize_dynamic_settings()
+
+	def intialize_dynamic_settings(self):
+		"""Initializing  settings who will be change in game time"""
+
+		self.ship_speed = 1.5
+		self.bullet_speed = 3.0
+		self.alien_speed = 1.0
+		self.fleet_direction = 1
+
+	def increase_speed(self):
+		"""Increasing speed settings"""
+
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
